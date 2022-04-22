b0765d61b42b9f5c201de663f8491f75df99fe38
gong.hanjin <richardgong1988@gmail.com>
2022-04-23 00:05:49 +0900

    minesweeper

--- codesignal/Main.go
+++ codesignal/Main.go
@@ -1,11 +1,34 @@
 package codesignal
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Main() {
-	image := Minesweeper([][]bool{
-		{false, false, false},
-		{false, false, false},
-	})
-	fmt.Println(image)
+	i := solution([]int{2, 4, 7})
+	fmt.Println(i)
+}
+
+func solution(a []int) int {
+	arrLen := len(a)
+	if arrLen <= 1 {
+		return 0
+	}
+
+	min := math.MaxFloat64
+	var ret int
+	for i := 0; i < arrLen; i++ {
+		var innerSum float64
+		for j := 0; j < arrLen; j++ {
+			innerSum += math.Abs(float64(a[i] - a[j]))
+		}
+
+		if innerSum < min {
+			min = innerSum
+			ret = a[i]
+		}
+	}
+	return ret
+
 }
---
+++ codesignal/stringsRearrangement.go
@@ -0,0 +1,5 @@
+package codesignal
+
+func bt(inputArray []string) {
+
+}
