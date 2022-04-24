7d73da46d9561b15bdc72d11c8056c0b55402776
gong.hanjin <richardgong1988@gmail.com>
2022-04-24 23:37:08 +0900

    minesweeper

--- codesignal/Main.go
+++ codesignal/Main.go
@@ -2,9 +2,47 @@ package codesignal
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func Main() {
-	i := RemoveKthElement([]int{1, 1, 1, 1, 1}, 1)
-	fmt.Println(i)
+	fmt.Println(FileNaming([]string{
+		"a(1)",
+		"a(6)",
+		"a",
+		"a"}))
+}
+
+func solution(names []string) []string {
+	m := map[string]int{}
+	var str []string
+	for _, name := range names {
+		result, exist := m[name]
+		if exist {
+			if result == 1 {
+				key := name + "(" + strconv.Itoa(result) + ")"
+				result2, exist2 := m[key]
+				if exist2 {
+					key2 := name + "(" + strconv.Itoa(result2+1) + ")"
+					m[key2]++
+					m[name]++
+					str = append(str, key2)
+				} else {
+					m[key]++
+					m[name]++
+					str = append(str, key)
+
+				}
+			} else if result > 1 {
+				key := name + "(" + strconv.Itoa(result) + ")"
+				m[key]++
+				m[name]++
+				str = append(str, key)
+			}
+		} else {
+			str = append(str, name)
+			m[name]++
+		}
+	}
+	return str
 }
---
+++ codesignal/fileNaming.go
@@ -0,0 +1,26 @@
+package codesignal
+
+import "strconv"
+
+func FileNaming(names []string) []string {
+
+	for i, name := range names {
+		if Contains(names[:i], name) {
+			j := 1
+			for Contains(names[:i], names[i]+"("+strconv.Itoa(j)+")") {
+				j++
+			}
+			names[i] += "(" + strconv.Itoa(j) + ")"
+		}
+	}
+	return names
+}
+
+func Contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
