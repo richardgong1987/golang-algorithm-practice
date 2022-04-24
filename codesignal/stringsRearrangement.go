738ee76e909c48d936be74bee734af20920eb251
gong.hanjin <richardgong1988@gmail.com>
2022-04-24 23:37:19 +0900

    minesweeper

--- codesignal/stringsRearrangement.go
+++ codesignal/stringsRearrangement.go
@@ -1,5 +1,29 @@
 package codesignal
 
-func bt(inputArray []string) {
+func StringsRearrangement(inputArray []string) bool {
+	inputLen := len(inputArray)
 
+	for i, iStr := range inputArray {
+		tmp := []string{iStr}
+		for j, jStr := range inputArray {
+			lastOne := tmp[len(tmp)-1]
+			if i != j && exactOneDiff(lastOne, jStr) {
+				tmp = append(tmp, jStr)
+			}
+		}
+		if inputLen-1 == len(tmp) {
+			return true
+		}
+	}
+	return false
+}
+func exactOneDiff(a string, b string) bool {
+	aLen := len(a)
+	diff := 0
+	for i := 0; i < aLen; i++ {
+		if a[i] != b[i] {
+			diff++
+		}
+	}
+	return diff == 1
 }
